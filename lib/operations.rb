4fd7e938571b13f6d0577e834a6092a02b385412
Your Username <you@example.com>
2020-10-17 20:10:13 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,20 @@
+require 'pry'
 def unsafe?(speed)
-
+        if speed < 40 || speed > 60 
+            return true
+        elsif speed == 40 || 60
+            return false
+        
+        
+    end
+    # binding.pry 
 end
 
 
 
 def not_safe?(speed)
-	
+    speed < 40 || speed > 60 ? true : false
+
 end
 	
 
